92f7bdc0ab1093e07d1c33c1d2a2de6ce66badca
Mike C. Fletcher <mcfletch@sturm>
2009-02-10 22:22:07 -0500

    Setup script for distribution

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""Installs SquareMap using distutils
+
+Run:
+	python setup.py install
+to install the package from the source archive.
+"""
+try:
+    from setuptools import setup
+except ImportError, err:
+    from distutils.core import setup
+
+version = [
+    (line.split('=')[1]).strip().strip('"').strip("'")
+    for line in open('version.py')
+    if line.startswith( '__version__' )
+][0]
+
+if __name__ == "__main__":
+	extraArguments = {
+		'classifiers': [
+			"""License :: OSI Approved :: BSD License""",
+			"""Programming Language :: Python""",
+			"""Topic :: Software Development :: Libraries :: Python Modules""",
+			"""Intended Audience :: Developers""",
+		],
+		'keywords': 'wxPython,squaremap',
+		'long_description' : """Hierarchic visualization control for wxPython 
+
+Hierarchic data visualization control intended for use with 
+structures where "parents" hold collections of weighted children.
+
+Can be used for viewing sizes of directories on file-systems,
+running-time of functions in profiling, or time spent on tasks 
+in a time-tracking application.""",
+		'platforms': ['Any'],
+	}
+	### Now the actual set up call
+	setup (
+		name = "SquareMap",
+		version = version,
+		url = "https://launchpad.net/squaremap",
+		description = "Hierarchic data-visualisation control for wxPython",
+		author = "Mike C. Fletcher",
+		author_email = "mcfletch@vrplumber.com",
+		license = "BSD",
+		package_dir = {
+			'squaremap':'.',
+		},
+		packages = [
+			'squaremap',
+		],
+		options = {
+			'sdist':{'force_manifest':1,'formats':['gztar','zip'],},
+		},
+		zip_safe=False,
+		**extraArguments
+	)
+
---
+++ version.py
@@ -0,0 +1,2 @@
+"""Declares the current version for use in setuptools and the like"""
+__version__ = '1.0.0b1'
